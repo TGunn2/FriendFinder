461ec978aaa2c681c42ec69ac4da72feb1d64e07
Pengwei Tian <tgun2w@gmail.com>
2019-10-14 22:39:29 -0700

    putSurveyandRoutesin2ndTry

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 1111;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+// =============================================================
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./app/public/home.html"));
+});
+
+app.get("/survey.html", function (req, res) {
+    res.sendFile(path.join(__dirname, "./app/public/survey.html"));
+});
+
+app.listen(PORT, () => {
+    console.log('App listening on PORT' + PORT);
+})
